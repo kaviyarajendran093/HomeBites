5eec35f1a40ab5ad3bedd14d43ac13a0f6a18cf2
kaviyaRaj <kaviyaraj1193@gmail.com>
2024-10-19 12:58:52 -0400

    set header folder

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div class="wrapper">
+        <div class="wrap">
+          <header>
+            <div class="logo"></div>
+            <nav>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </nav>
+          </header>
+
+          <div class="arrow animated bounce"></div>
+
+          <section class="transparent">
+            <div class="headline"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="button"></div>
+          </section>
+
+          <section class="cards">
+            <div class="card">
+              <img
+                src="https://imgplaceholder.com/500x300/transparent/ddd/ion-image"
+                alt=""
+              />
+              <div class="content">
+                <div class="headline"></div>
+                <div class="text"></div>
+                <div class="text"></div>
+                <div class="button"></div>
+              </div>
+            </div>
+            <div class="card">
+              <img
+                src="https://imgplaceholder.com/500x300/transparent/ddd/ion-image"
+                alt=""
+              />
+              <div class="content">
+                <div class="headline"></div>
+                <div class="text"></div>
+                <div class="text"></div>
+                <div class="button"></div>
+              </div>
+            </div>
+            <div class="card">
+              <img
+                src="https://imgplaceholder.com/500x300/transparent/ddd/ion-image"
+                alt=""
+              />
+              <div class="content">
+                <div class="headline"></div>
+                <div class="text"></div>
+                <div class="text"></div>
+                <div class="button"></div>
+              </div>
+            </div>
+          </section>
+
+          <section class="contentbox">
+            <div class="headline"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="text"></div>
+            <div class="button"></div>
+          </section>
+        </div>
+      </div>
+
+      <footer>
+        <div class="content">
+          <div class="logo"></div>
+          <nav>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </nav>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Header;
