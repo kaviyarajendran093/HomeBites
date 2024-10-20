7d03fc110fe5b75793c19f82a2c3b2e82d3a3b3c
kaviyaRaj <kaviyaraj1193@gmail.com>
2024-10-20 00:30:25 -0400

    set hero image and content, apply animation to hero image and play button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import "./Home.scss";
+import hero from "../../assets/Images/hero/heroImage.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <div className="hero">
-      <div className="hero__image">
-        <img />
+      <div className="hero__image-wrapper">
+        <img className="hero__image" src={hero} />
       </div>
-      <div className="hero__content"></div>
+      <div className="hero__content-wrapper">
+        <div className="hero__content">
+          <p className="hero__title">Enjoy Our Delicious Meal</p>
+          <p className="hero__description">
+            Craving the taste of home? Connect with local chefs who bring
+            authentic, homemade meals from your community straight to your door.
+            Enjoy fresh, home-cooked dishes crafted with love and delivered with
+            care.
+          </p>
+
+          <button className="hero__button">
+            <FontAwesomeIcon icon={faPlay} fade className="hero__play" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
