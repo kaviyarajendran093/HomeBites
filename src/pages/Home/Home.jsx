d255a52d4d5a4ede43f607f72dca68e752b428b1
kaviyaRaj <kaviyaraj1193@gmail.com>
2024-10-19 21:14:19 -0400

    set link and logo to header

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="hero__image">
+        <img />
+      </div>
+      <div className="hero__content"></div>
+    </div>
+  );
+};
+
+export default Home;
